90fc6de8ffeec779ddbb59163cb980537a34e0cd
Saurabh-kushwaha <kushwahasaurabh219@gmail.com>
2026-05-05 21:52:20 +0530

    server file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,27 @@
 // src/server.ts
-import app from "./app";
-import dotenv from "dotenv";
+// import app from "./app";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+console.log("Server is running...");
+
+// closer
+
+function counter() {
+  let count = 0;
+  return function () {
+    count++;
+    console.log(count);
+  }
+}
+
+const increment = counter();
+increment(); // 1
+increment();
